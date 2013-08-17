2dd91e5cc3628257056247646e0727db5ce9777c
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-17 18:24:54 +0100

    more tests

--- test/link-request.js
+++ test/link-request.js
@@ -1,4 +1,3 @@
-
 var link = require('../')
 var tape = require('tape')
 var leaves  = require('npmd-leaves')
@@ -8,14 +7,14 @@ function first (obj) {
     return obj[k]
 }
 
-tape('install ws', function (t) {
-  var tree = require('./ws.json')
+tape('install request', function (t) {
+  var tree = require('./request-tree.json')
   link.all(tree, {path: __dirname}, function (err) {
     if(err) throw err
-    var wsPkg = require('./node_modules/ws/package.json')
+    var requestPkg = require('./node_modules/request/package.json')
     var root = first(leaves.roots(tree))
-    t.equal(wsPkg.name, root.name)
-    t.equal(wsPkg.version, root.version)
+    t.equal(requestPkg.name, root.name)
+    t.equal(requestPkg.version, root.version)
     t.end()
   })
 })
